102d63c30d93cff5042e1e661d1d9dc87b2fd505
Hisam Uddin <hisam.uddin844@gmail.com>
2026-07-25 13:36:31 +0600

    Add fallback for POSTGRES_URL

--- api/db.js
+++ api/db.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 const { Pool } = require('@neondatabase/serverless');
 
-// We expect the Neon Postgres connection string in the DATABASE_URL environment variable.
+// We expect the Neon Postgres connection string in the DATABASE_URL or POSTGRES_URL
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL || process.env.POSTGRES_URL,
 });
 
 module.exports = {
